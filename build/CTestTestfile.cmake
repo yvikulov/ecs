d140b545c773b5545af617085e71532ca7ebae8c
Yehor Vikulov <20969918+yvikulov@users.noreply.github.com>
2022-08-25 00:53:26 +0300

    clean up

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yvikulov/Desktop/ecs
-# Build directory: /Users/yvikulov/Desktop/ecs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
